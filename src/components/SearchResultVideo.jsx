1aa6175c0a6ea657dd0bbfeaa2e40996d4ba8a5c
fiddicode <coderfardeen@gmail.com>
2023-07-22 12:19:29 +0530

    modified:   src/components/SearchResult.jsx 	deleted:    src/components/SearchResultVideo.jsx 	new file:   src/components/SearchResultVideoCard.jsx

--- src/components/SearchResultVideo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SearchResultVideo = () => {
-  return (
-    <div>SearchResultVideo</div>
-  )
-}
-
-export default SearchResultVideo
---
+++ src/components/SearchResultVideoCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SearchResultVideoCard = () => {
+  return (
+    <div>SearchResultVideoCard
+    </div>
+  )
+}
+
+export default SearchResultVideoCard;
