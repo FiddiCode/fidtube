3c889582c5ce591f35dea7244acb17ea089e4f16
fiddicode <coderfardeen@gmail.com>
2023-09-22 11:42:55 +0530

    modified:   src/components/LeftNav.jsx

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -26,7 +26,7 @@ const LeftNav = () => {
 
     return (
         <div
-            className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${mobileMenu ? "translate-x-1" : "" }`}
+            className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${mobileMenu ? "translate-x-2" : "" }`}
         >
             <div className="flex px-5 flex-col">
                 {categories.map((item) => {
