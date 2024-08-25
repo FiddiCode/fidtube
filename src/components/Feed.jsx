193b042f088580163b98e8c26fde364881f6af55
Fardeen Qureshi <coderfardeen@gmail.com>
2024-08-25 22:13:02 +0530

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,7 @@
 import React, { useContext , useEffect } from "react";
 
 import { Context } from "../context/contextApi";
-import LeftNav from "./LeftNav";
+import LeftNav from "./LeftNav" ;
 import VideoCard from "./VideoCard";
 
 const Feed = () => {
